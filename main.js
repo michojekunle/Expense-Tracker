be3ac9e47a9893515ed1685adc4d6e6c34200d01
AMD <michojekunle1@gmail.com>
2022-07-07 10:17:01 +0100

    lniking script files done with most functions adding event listeners

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ const amountInput = document.querySelector('#amount-input');
 const submitTransactionBtn = document.querySelector('#submit-transaction');
 const deleteButton = document.querySelector('.delete-transaction');
 
-//creating histroy element
-let history = document.createElement('div');
+//declaring history
+let history;
 
 //declaring mainArray containing amount of all transactions.
 const mainArray = [];
@@ -25,6 +25,7 @@ function calculateIncome(incomeArray) {
     return total;    
 }//end function calculateIncome.
 
+
 //function to calculte expense.
 function calculateExpense(expenseArray) {
     let total = 0;
@@ -33,6 +34,7 @@ function calculateExpense(expenseArray) {
     })    
     return total;
 } //end function calaculateExpense
+
 
 //function to calculate Balance
 function calculateBalance(mainArray) {
@@ -57,8 +59,49 @@ function calculateBalance(mainArray) {
     expenseEl.textContent = `₦${expense}`;   
 } // done with calculating balance function.
 
+
 //function to add to History 
 function addToHistory(transaction, amount) {
+    //creating histroy element
+    history = document.createElement('div');
+    history.classList.add('in-ex');
 
-    
-}
+    if (amount > 0) {
+        history.classList.add('income');
+    } else {
+        history.classList.add('expense');
+    }
+       
+    history.innerHTML = `
+    <button id="delete-transaction">X</button>
+    <div class="content">
+        <p>${transaction}</p>
+        <p>${amount}</p>
+    </div>`
+} // end addToHistory
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (transactionInput==='' || amountInput==='') {
+        alert('Please fill in values for transaction and amount')
+    }
+})
+
+
+history.addEventListener('hover', () => {
+    deleteButton.classList.add('show');
+});
+
+deleteButton.addEventListener('click', () => {
+    let amount = deleteButton.nextElementSibling.lastChild.textContent;
+    for(let i=0; 1<mainArray.length; i++){
+        if(mainArray[i] === amount){
+            mainArray.splice(i, 1);
+            break;
+        }    
+    }
+    calculateBalance(mainArray);
+
+    deleteButton.parentElement.remove();
+});
+
