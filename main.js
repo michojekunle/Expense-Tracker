692b1740b1d67957eff197e593b92ce444b0d194
AMD <michojekunle1@gmail.com>
2022-07-07 11:58:23 +0100

    wanted to start local storage functionalty

--- main.js
+++ main.js
@@ -16,6 +16,40 @@ let history;
 //declaring mainArray containing amount of all transactions.
 const mainArray = [];
 
+//Store class:handles storage
+// class Store {
+//     static getBooks() {
+//         let books;
+
+//         if(localStorage.getItem('books')===null){
+//             books = [];
+//         } else {
+//             books = JSON.parse(localStorage.getItem('books'));
+//         }
+//         return books;
+//     }
+    
+//     static addBook(book) {
+//         const books = Store.getBooks();
+
+//         books.push(book);
+
+//         localStorage.setItem('books', JSON.stringify(books));
+//     }
+
+//     static removeBook(isbn) {
+//         const books = Store.getBooks();
+
+//         books.forEach((book, index) => {
+//             if (book.isbn === isbn){
+//                 books.splice(index, 1);
+//             }
+//         })
+
+//         localStorage.setItem('books', JSON.stringify(books));
+//     }
+// }
+
 //function to calculate income.
 function calculateIncome(incomeArray) {
     let total = 0;
